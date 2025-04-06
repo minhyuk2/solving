b9297baf3ad3cf993b2080cf94abe6bfee4c126a
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-06 16:13:10 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(void){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    if(N == 0){
+        cout << "YONSEI" << "\n";
+    }else{
+        cout << "Leading the Way to the Future\n";
+    }
+}
