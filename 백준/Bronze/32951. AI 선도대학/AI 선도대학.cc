7c838c353a9699e8747f3c50446f93606ce37e82
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-19 14:45:50 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int N;
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N;
+
+    cout << N-2024 << "\n";
+
+}
