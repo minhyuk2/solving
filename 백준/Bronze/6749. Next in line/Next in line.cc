1a96c8711165b550e84e20e9ef447fae5715b9e7
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-04-21 00:20:14 +0900

    [Bronze IV] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N , M;
+    cin >> N >> M;
+
+    cout << M+(M-N) << "\n";
+}
