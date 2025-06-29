8d0b7e84c4fd790c4c73bf00f2ddef936ca64ab8
조민혁 <71816821+minhyuk2@users.noreply.github.com>
2025-06-30 04:17:36 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 292 ms, Memory: 27000 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+set<int,greater<int>> sets[100005];
+int arr[100005];
+bool visited[100005];
+
+int N , M ,St;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> N >> M >> St;
+
+    int temp1, temp2;
+    for(int i= 0; i<M;i++){
+        cin >> temp1 >> temp2;
+        sets[temp1].insert(temp2);
+        sets[temp2].insert(temp1);
+    }
+
+    queue<int> que;
+    que.push(St);
+    int  count = 1;
+
+    while(!que.empty()){
+        int now = que.front();
+        que.pop();
+
+        if(visited[now] == 0){
+            visited[now] = 1;
+            arr[now] = count;
+            count ++;
+        
+
+            for(auto it : sets[now] ){
+                que.push(it);
+            }
+        }
+
+    }
+
+    for(int i =1;i<=N;i++){
+        cout << arr[i] << "\n";
+    }
+
+}
